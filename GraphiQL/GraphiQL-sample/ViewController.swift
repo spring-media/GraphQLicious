984b60d162bb6b8a2d4ebfa9d01aa59cefae8d64
Felix Dietz <felix.dietz@weltn24.de>
2016-03-30 17:02:03 +0200

    initial commit

---
+++ GraphiQL/GraphiQL-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GraphiQL-sample
+//
+//  Created by Felix Dietz on 30/03/16.
+//  Copyright © 2016 WeltN24. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ GraphiQL/GraphiQL/GraphiQL.h
@@ -0,0 +1,19 @@
+//
+//  GraphiQL.h
+//  GraphiQL
+//
+//  Created by Felix Dietz on 30/03/16.
+//  Copyright © 2016 WeltN24. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GraphiQL.
+FOUNDATION_EXPORT double GraphiQLVersionNumber;
+
+//! Project version string for GraphiQL.
+FOUNDATION_EXPORT const unsigned char GraphiQLVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GraphiQL/PublicHeader.h>
+
+
